0837d9902f0e7d19bf29b288d8ef64ce4687ffea
GVALFER <106562911+GVALFER@users.noreply.github.com>
2024-03-01 02:11:11 +0000

    Update admin.js

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { authProtect } = require("../utils/auth");
 
-// Define a route for getting all users
 router.get("/", authProtect(), (req, res) => {
     res.send("This route is protected");
 });
